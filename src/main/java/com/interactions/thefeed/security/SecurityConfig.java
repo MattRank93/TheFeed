3d35e39f6b458706be4068ecb96bc1cffbc1260f
workmatt <matthew@zetaton.com>
2022-08-16 12:07:39 -0500

    functioning test for auth

--- src/main/java/com/interactions/thefeed/security/SecurityConfig.java
+++ src/main/java/com/interactions/thefeed/security/SecurityConfig.java
@@ -1,45 +1,45 @@
-package com.interactions.thefeed.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and().csrf().disable();
-
-
-
-//        httpSecurity
-//                .csrf().disable()
-//                .authorizeRequests().antMatchers("/api/**").permitAll()
-//                .antMatchers("/**").authenticated()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .httpBasic()
-//                .and()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-    }
-
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("{noop}password")
-                .roles("ADMIN");
-    }
-
-
-
-}
+//package com.interactions.thefeed.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    public void configure(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and().csrf().disable();
+//
+//
+//
+////        httpSecurity
+////                .csrf().disable()
+////                .authorizeRequests().antMatchers("/api/**").permitAll()
+////                .antMatchers("/**").authenticated()
+////                .anyRequest()
+////                .authenticated()
+////                .and()
+////                .httpBasic()
+////                .and()
+////                .sessionManagement()
+////                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+//    }
+//
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("admin")
+//                .password("{noop}password")
+//                .roles("ADMIN");
+//    }
+//
+//
+//
+//}
